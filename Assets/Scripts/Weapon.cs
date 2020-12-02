816beac6d3d180e275162e31d71480cc745cf015
good <good@good.yoi.home.blog>
2020-12-02 23:14:43 +0800

    weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{	
+	public string weaponName;
+	public Transform firePoint;
+	public Vector3 positionOffset;
+	public Vector3 rotationOffset;
+	public GameObject bulletPrefab;
+	public float fireDelay = 0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	transform.localPosition = positionOffset;    
+    	transform.localRotation = Quaternion.Euler(rotationOffset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public Transform handTransform;
+    public List<MyWeapon> myWeapons;
+
+    MyWeapon curWeapon;
+
+    float curTime = 0f;
+    void Update(){
+    	if(Input.GetKeyDown(KeyCode.Alpha1)){
+    		selectWeapon(0);
+    	}
+
+    	if(Input.GetKeyDown(KeyCode.Alpha2)){
+    		selectWeapon(1);
+    	}
+
+    	if(Input.GetButton("Fire1")){
+    		fireWeapon();
+    	} else {
+    		curTime = 0f;
+    	}
+    }
+
+    public void selectWeapon(int index){
+    	
+    	MyWeapon w = myWeapons[index];
+    	if(curWeapon == null || w.weapon.weaponName != curWeapon.weapon.weaponName){
+    		if(curWeapon != null){
+	    		Destroy(curWeapon.weapon.gameObject);
+	    	}
+    		GameObject wp = Instantiate(w.weapon.gameObject,handTransform.position,Quaternion.identity);
+	    	wp.transform.SetParent(handTransform);
+	    	curWeapon = w;
+	    	curWeapon.weapon = wp.GetComponent<Weapon>();
+    	}
+    }
+
+    
+    public void fireWeapon(){
+    	curTime += Time.deltaTime;
+    	if(curTime > curWeapon.weapon.fireDelay){
+    		var b = Instantiate(curWeapon.weapon.bulletPrefab,curWeapon.weapon.firePoint.position,Quaternion.identity);
+    		b.GetComponent<Rigidbody>().AddForce(0,0,100f);
+    		curTime = 0;
+    	}
+
+    }
+
+
+}
+
+[Serializable]
+public class MyWeapon
+{
+	public Weapon weapon;
+	public int weaponAmmo;
+}
+
+
+// [Serializable]
+// class WeaponUse
+// {
+// 	public string weaponName;
+// 	public GameObject weaponPrefab;
+// }
+
+
